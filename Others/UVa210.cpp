80b30e63a84b0556ba3ac5542e20802fac65615c
Dasheverless <61368791+Dasheverless@users.noreply.github.com>
2021-04-25 23:06:08 +0800

    Create UVa210.cpp

---
+++ Others/UVa210.cpp
@@ -0,0 +1,139 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <deque>
+#include <string>
+using namespace std;
+
+int stm_process(string s)
+{
+    switch (s[2])
+    {
+        case '=':return 0;//赋值
+        break;
+        case 'i':return 1;//打印
+        break;
+        case 'c':return 2;//锁定
+        break;
+        case 'l':return 3;//解锁
+        break;
+        case 'd':return 4;//end
+        break;
+        default:return -1;
+        break;
+    }
+}
+int get_value(string s)
+{
+    int value = 0;
+    for (int i = 4; i < s.length(); i++)
+    {
+        value = (value*10 + s[i]-'0');
+    }
+    return value;
+}
+int get_var(string s)
+{
+    return s[6];
+}
+int main()
+{
+    int T;
+    cin >> T;
+    while(T--)//VOJ没有说明有测试用例数，卡了好久WA
+    {
+        int n, t[5], Q, vars[26] = {0};
+        vector< vector<string> > Program;
+        vector<string> statements;
+        cin >> n;
+        vector< int > cur_statement(n,0);
+        for (int i = 0; i < 5; i++)
+        {
+            cin >> t[i];
+        }
+        cin >> Q;
+        int Pcount = 0, Scount = 0;
+        string temp;
+        deque<int> wait_queue;
+        queue<int> block_queue;
+        cin.get();
+        while(Pcount < n)
+        {
+            getline(cin,temp);
+            if (temp != "end")
+            {
+                statements.emplace_back(temp);
+                Scount ++;
+            }
+            else
+            {
+                statements.emplace_back(temp);
+                Program.emplace_back(statements);
+                statements.clear();
+                wait_queue.push_back(Pcount);
+                Pcount ++;
+            }
+        }
+        int P_id, cur_t = 0;
+        bool  locked = false;
+        while(!wait_queue.empty())
+        {
+            P_id = wait_queue.front();
+            wait_queue.pop_front();
+            wait_queue.push_back(P_id);
+            cur_t = 0;
+            while(cur_t < Q)
+            {
+                string statement = Program[P_id][cur_statement[P_id]];
+                int type = stm_process(statement);
+                if (type == 2)
+                {
+                    //lock
+                    if (locked)
+                    {
+                        block_queue.push(P_id);
+                        wait_queue.pop_back();
+                        break;
+                    }
+                    else
+                    {
+                        locked = true;
+                    }
+                }
+                else if (type == 3)
+                {
+                    //unlock
+                    locked = false;
+                    if (!block_queue.empty())
+                    {
+                        wait_queue.push_front(block_queue.front());
+                        block_queue.pop();
+                    }
+                }
+                else if(type == 4)
+                {
+                    //end
+                    wait_queue.pop_back();
+                    break;
+                }
+                else if (type == 0)
+                {
+                    //assignment
+                    int pos = statement[0] - 'a';
+                    vars[pos] = get_value(statement);
+                }
+                else if(type == 1)
+                {
+                    //print
+                    int pos = get_var(statement) - 'a';
+                    printf("%d: %d\n",P_id+1,vars[pos]);
+                }
+                cur_statement[P_id] ++;
+                cur_t += t[type];
+            }
+        }
+        if(T)
+            printf("\n");
+    }
+    return 0;
+}
